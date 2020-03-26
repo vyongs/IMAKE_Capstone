934e3a324bc721f15a01887089a73e18cb52426f
Euna789 <55429220+Euna789@users.noreply.github.com>
2020-03-26 12:40:30 +0900

    Update hough circle realtime.py

--- Drawing/hough transform/hough circle realtime.py
+++ Drawing/hough transform/hough circle realtime.py
@@ -10,16 +10,17 @@ while True:
     gray = cv2.medianBlur(gray,5)
     
     circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,150,param1=50,param2=35,minRadius=0,maxRadius=0)
-    for c in circles[0,:]:
+    if circles is not None:
+        for c in circles[0,:]:
 
-        center = (c[0],c[1])
-        radius = c[2]
+            center = (c[0],c[1])
+            radius = c[2]
     
         # 바깥원
-        cv2.circle(orig_frame,center,radius,(0,255,0),2)
+            cv2.circle(orig_frame,center,radius,(0,255,0),2)
     
         # 중심원
-        cv2.circle(orig_frame,center,2,(0,0,255),3)
+            cv2.circle(orig_frame,center,2,(0,0,255),3)
     cv2.imshow("frame", orig_frame)
     key = cv2.waitKey(1)
     if key == 27:
